e5688f1dc24348e8bda70b8c6c6b6bd0bfc23f0e
Sufyan Zaki <sufyan.zaki.789@gmail.com>
2023-09-10 20:53:22 +0500

    added css in ejs

--- controller/viewController.js
+++ controller/viewController.js
@@ -1,3 +1,11 @@
-export const homeView = (async (req, res, next) => {
-   res.render('index')
-  });
+export const homeView = async (req, res, next) => {
+  res.render("index");
+};
+
+export const loginView = async (req, res, next) => {
+  res.render("sign-in");
+};
+
+export const signupView = async (req, res, next) => {
+  res.render("sign-up");
+};
