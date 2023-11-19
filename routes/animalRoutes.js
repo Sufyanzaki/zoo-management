952d2c03ddd3f1f59bf420e83c74a2a7c8a13785
Sufyanzaki <43870607+Sufyanzaki@users.noreply.github.com>
2023-11-18 21:38:39 -0800

    UI fixes

--- routes/animalRoutes.js
+++ routes/animalRoutes.js
@@ -3,7 +3,7 @@ import {
     createAnimal, removeImages, updateAnimal, deleteAnimal
 } from "../controller/animalController.js";
 
-import { isAuthenticatedUser, authorizeRoles } from "../middleware/auth.js";
+import { isAuthenticatedUser } from "../middleware/auth.js";
 
 const router = express.Router();
 
